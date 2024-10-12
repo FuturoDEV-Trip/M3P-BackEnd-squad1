5c8a2bd1da77400df6b35530d79884d896a39c58
Alex Athayde Viana Ribeiro <alex_a_ribeiro@estudante.sesisenai.org.br>
2024-10-12 14:40:21 -0300

    refatorando rota e controller home

--- src/routes/home.routes.js
+++ src/routes/home.routes.js
@@ -3,9 +3,8 @@ const homeRoutes = new Router()
 const HomeController = require('../controllers/HomeController')
 const { auth } = require('../middleware/auth')
 
-homeRoutes.get('/totalUsuariosAtivos', HomeController.usuariosAtivos)
+homeRoutes.get('/usuariosAtivos', HomeController.usuariosAtivos)
 homeRoutes.put('/:id', auth, HomeController.logout)
-homeRoutes.get('/listarDestinos', HomeController.listarDestinos)
 homeRoutes.get('/totalDestinos', HomeController.totalDestinos)
 
 
