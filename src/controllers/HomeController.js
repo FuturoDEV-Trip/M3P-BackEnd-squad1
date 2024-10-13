b09e743b236ce3b3d81750c99a36e2250e3309cf
Jorge Herrera <jorgeluishm88@gmail.com>
2024-10-13 18:47:19 -0300

    otimização controllers destino e home

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -10,20 +10,42 @@ class HomeController {
         #swagger.description= 'Retorna o número de usuários que estão ativos (status = true).'
     */
     try {
-      
-      const { count, rows } = await Usuario.findAndCountAll({
+      const { count } = await Usuario.findAndCountAll({
         where: { status: true },
-        attributes: ['id', 'nome', 'email']
-      })
-    
-      return res.status(200).json({ usuariosAtivos: count, usuarios: rows})
+      });
+
+      return res.status(200).json({ usuariosAtivos: count });
     } catch (error) {
-      return res.status(500).json({ error: 'Erro ao contar usuários ativos na plataforma, tente novamente mais tarde.', error })
+      return res.status(500).json({
+        error:
+          "Erro ao contar usuários ativos na plataforma, tente novamente mais tarde.",
+        error,
+      });
+    }
+  }
+
+  async totalDestinos(req, res) {
+    /*
+        #swagger.path = '/totalDestinos',
+        #swagger.method = 'get',
+        #swagger.tags = ['Home'],
+        #swagger.description= 'Retorna a contagem total de destinos no sistema e a lista de destinos cadastrados na plataforma.'
+    */
+    try {
+      const { count } = await Destino.findAndCountAll();
+
+      return res.status(200).json({ totalDestinos: count });
+    } catch (error) {
+      return res.status(500).json({
+        error:
+          "Erro ao contar destinos cadastrados na plataforma, tente novamente mais tarde.",
+        error,
+      });
     }
   }
 
   async logout(req, res) {
-     /*
+    /*
         #swagger.path = '/:id',
         #swagger.method = 'put',    
         #swagger.tags = ['Home'],
@@ -31,37 +53,24 @@ class HomeController {
     */
     try {
       const { id } = req.params;
-      const usuario = await Usuario.findByPk(id)
+      const usuario = await Usuario.findByPk(id);
 
       if (!usuario) {
-        return res.status(404).json({ erro: 'Nenhum usuário cadastrado na plataforma com o id informado.' })
+        return res.status(404).json({
+          erro: "Nenhum usuário cadastrado na plataforma com o id informado.",
+        });
       }
 
-      usuario.status = false
-      await usuario.save()
+      usuario.status = false;
+      await usuario.save();
 
-      res.status(200).json({ message: 'Logout realizado com sucesso.' })
+      res.status(200).json({ message: "Logout realizado com sucesso." });
     } catch (error) {
-      res.status(500).json({ error: 'Erro ao realizar o logout, tente novamente mais tarde.' })
-    }
-  }
-
-  async totalDestinos(req, res) {
-     /*
-        #swagger.path = '/totalDestinos',
-        #swagger.method = 'get',
-        #swagger.tags = ['Home'],
-        #swagger.description= 'Retorna a contagem total de destinos no sistema e a lista de destinos cadastrados na plataforma.'
-    */
-    try {
-      
-      const { count, rows } = await Destino.findAndCountAll()
-    
-      return res.status(200).json({ totalDestinos: count, destinos: rows})
-    } catch (error) {
-      return res.status(500).json({ error: 'Erro ao contar destinos cadastrados na plataforma, tente novamente mais tarde.', error })
+      res.status(500).json({
+        error: "Erro ao realizar o logout, tente novamente mais tarde.",
+      });
     }
   }
 }
 
-module.exports = new HomeController()
+module.exports = new HomeController();
