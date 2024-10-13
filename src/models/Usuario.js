1051d8a7a6a819aaadcb241c36e61fcb5aab857c
Jorge Herrera <jorgeluishm88@gmail.com>
2024-10-13 18:20:53 -0300

    modificação chamdas da api na DB - otimização codigo

--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -1,61 +1,65 @@
-const { DataTypes } = require('sequelize')
-const { connection } = require('../database/connection')
+const { DataTypes } = require("sequelize");
+const { connection } = require("../database/connection");
+const Destino = require("./Destino");
 
-const Usuario = connection.define('usuarios', {
-    nome: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    sexo: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    cpf: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
-    },
-    data_nascimento: {
-        type: DataTypes.DATEONLY,
-        allowNull: false
-    },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    cep: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    endereco: {
-        type: DataTypes.STRING,
-        allowNull: true
-    },
-    numero: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    bairro: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    cidade: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    estado: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    status: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false
-    }
-})
+const Usuario = connection.define("usuarios", {
+  nome: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  sexo: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  cpf: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  data_nascimento: {
+    type: DataTypes.DATEONLY,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  cep: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  endereco: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  numero: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  bairro: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  cidade: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  estado: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  status: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+});
 
-module.exports = Usuario
+Usuario.hasMany(Destino, { foreignKey: "usuario_id", sourceKey: "id" });
+Destino.belongsTo(Usuario, { foreignKey: "usuario_id", targetId: "id" });
+
+module.exports = Usuario;
