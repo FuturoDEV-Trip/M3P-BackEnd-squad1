8ae5677ec362bf3255bb586d117e670a1ad01c66
karinaperes <karinaperesg@gmail.com>
2024-10-14 14:14:57 -0300

    Configuração security

--- src/server.js
+++ src/server.js
@@ -15,9 +15,9 @@ class Server {
 
     async middlewares(app) {
         app.use((req, res, next) => {
-            res.setHeader("Content-Security-Policy", "default-src 'self'; script-src 'self' https://translate.yandex.net;");
+            res.setHeader("Content-Security-Policy", "default-src 'self'; script-src 'self' https://translate.yandex.net https://yastatic.net;");
             next();
-        })
+        });
 
         app.use(cors({
             origin: 'https://m3p-frontend-squad1-nxcl.onrender.com',
