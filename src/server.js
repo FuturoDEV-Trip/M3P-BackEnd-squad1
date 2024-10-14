10f143cc391b9abfdd68af7a571a5abe1566e2bb
karinaperes <karinaperesg@gmail.com>
2024-10-14 13:41:07 -0300

    Configuração cors

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const routes = require('./routes/routes')
 const PORT_API = process.env.PORT_API
 
 class Server {
-    constructor (server = express()) {
+    constructor(server = express()) {
         this.middlewares(server)
         this.database()
         server.use(routes)
@@ -14,14 +14,18 @@ class Server {
     }
 
     async middlewares(app) {
-        app.use(cors())
+        app.use(cors({
+            origin: 'https://m3p-frontend-squad1-nxcl.onrender.com',
+            methods: ['GET', 'POST', 'PUT', 'DELETE'],
+            allowedHeaders: ['Content-Type', 'Authorization'],
+        }))
         app.use(express.json())
     }
 
     async database() {
         try {
             await connection.authenticate()
-            console.log('Conexão com o banco de dados PostgreSQL foi estabelecida com sucesso.')            
+            console.log('Conexão com o banco de dados PostgreSQL foi estabelecida com sucesso.')
         } catch (error) {
             console.error('Não foi possível conectar o banco de dados', error)
             throw error
@@ -33,4 +37,4 @@ class Server {
     }
 }
 
-module.exports = {Server}
+module.exports = { Server }
